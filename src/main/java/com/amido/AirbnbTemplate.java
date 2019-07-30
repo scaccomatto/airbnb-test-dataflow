2aa957660deb01fbb662da89850219bdf301eae2
matteoparussini <matteo.parussini@amido.com>
2019-07-30 11:15:39 +0100

    update getOutput

--- src/main/java/com/amido/AirbnbTemplate.java
+++ src/main/java/com/amido/AirbnbTemplate.java
@@ -2,12 +2,15 @@ package com.amido;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.Top;
 import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.options.Validation.Required;
+import org.apache.beam.sdk.values.PCollection;
 
 import java.util.List;
 
@@ -22,22 +25,14 @@ public class AirbnbTemplate {
 
         Pipeline pipeline = Pipeline.create(options);
 
+//        PCollection allAirbnbProperties = pipeline.apply("ReadLines", TextIO.read().from(options.getInputFile()));
+
         pipeline.apply("ReadLines", TextIO.read().from(options.getInputFile()))
-                .apply("GetPriceById", ParDo.of(new DoFn<String, KV<String,Integer>>() {
+                .apply("GetPriceById", ParDo.of(new AirbnbPropertyPrice()))
+                .apply("Top_5", Top.of(5, new KV.OrderByValue<>()))
+                .apply("PrintOut", ParDo.of(new DoFn<List<KV<String, Integer>>, String>() {
                     @ProcessElement
-                    public void processElement(ProcessContext c) throws Exception {
-                        String[] airbnbPorperty = c.element().split(",");
-                        if(airbnbPorperty.length>=9 && airbnbPorperty[9].matches("\\d+")){
-                            c.output(KV.of(airbnbPorperty[0],Integer.valueOf(airbnbPorperty[9])));
-                        }
-
-                    }
-                }))
-                .apply("Top_5", Top.of(5, new KV.OrderByValue<>())) //
-                .apply("ToString", ParDo.of(new DoFn<List<KV<String, Integer>>, String>() {
-
-                    @ProcessElement
-                    public void processElement(ProcessContext c) throws Exception {
+                    public void processElement(ProcessContext c) {
                         StringBuffer sb = new StringBuffer();
                         for (KV<String, Integer> kv : c.element()) {
                             sb.append(kv.getKey() + "," + kv.getValue() + '\n');
@@ -45,27 +40,46 @@ public class AirbnbTemplate {
                         c.output(sb.toString());
                     }
 
-                })) //
-                .apply(TextIO.write().to(options.getOutput()+"top5"));
+                }))
+                .apply(TextIO.write().to(options.getOutput() + "top5"));
 
         pipeline.run().waitUntilFinish();
 
     }
 
+    /*
+    * Matching just the id and the price of the property
+    * TODO: need to add validation and extends matching properties
+    * */
+    private static class AirbnbPropertyPrice extends DoFn<String, KV<String, Integer>> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String[] airbnbPorperty = c.element().split(",");
+            if (airbnbPorperty.length >= 9 && airbnbPorperty[9].matches("\\d+")) {
+                c.output(KV.of(airbnbPorperty[0], Integer.valueOf(airbnbPorperty[9])));
+            }
+        }
+    }
 
+    /*
+    * Adding some default options to the pipeline
+    * Google Storage is used as default storage
+    * */
     public interface AirbnbTemplateOptions extends PipelineOptions {
 
         @Description("Path of the file to read from")
-        @Default.String("gs://demo_aribnb_londra/data/listings.csv")
+        @Default.String("gs://demo_aribnb_londra/data/*.csv")
         String getInputFile();
 
         void setInputFile(String value);
 
-        /** Set this required option to specify where to write the output. */
+        /**
+         * Set this required option to specify where to write the output.
+         */
 
         //@Default.String("gs://demo_aribnb_londra/output/")
         @Description("Path of the file to write to")
-        @Required
+        @Default.String("gs://demo_aribnb_londra/output/")
         String getOutput();
 
         void setOutput(String value);
